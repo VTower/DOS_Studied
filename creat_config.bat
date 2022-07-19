629268883f27c6ea936e707150d1dcc1b42cddd4
Victor Dalla Torre <83186077+VTower@users.noreply.github.com>
2022-07-18 23:22:15 -0300

    Use to create a temporary file and use config

---
+++ creat_config.bat
@@ -0,0 +1,29 @@
+@echo off
+
+if exist config.bat (call config.bat) else (goto cria)
+
+color %cor_fundo%%cor_letra%
+
+title %titulo%
+
+echo Este e o resultado final !
+
+pause>nul
+
+
+:cria
+echo PAINEL DE PROPRIEDADES:
+echo.
+set /p "cor_letra=Digite uma Cor de letra 1-9>"
+echo.
+set /p "cor_fundo=Digite uam Cor de fundo A-F>"
+echo.
+set /p "titulo=Digite o Titulo da Janela>"
+
+msg /w * "Abra esse script novamente para ver o resultado!"
+
+(
+echo set cor_letra=%cor_letra%
+echo set cor_fundo=%cor_fundo%
+echo set titulo=%titulo%
+)>config.bat
