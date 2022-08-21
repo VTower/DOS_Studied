8ba2c7a805262822628c7ba8ac55c24d5e6c6e51
Victor Dalla Torre <83186077+VTower@users.noreply.github.com>
2022-08-20 23:17:54 -0300

    Add files via upload

--- guias_abertas.cmd
+++ guias_abertas.cmd
@@ -6,3 +6,6 @@ tasklist | find /i "opera.exe">"%userprofile%/Desktop/saida.txt"
 FOR /D %%"opera.exe" IN (saida.txt) DO (echo vitoria && pause)
 
 
+---fazer p for separar por espa;os de nomes
+---contar quantas vezes ele faz isso 
+---pritnar com o nome e um tamanho legal ( movimenta;ao de telinha)
